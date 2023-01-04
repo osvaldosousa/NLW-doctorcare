ad997f5c9cf47d314f3a6b242fd8c48ca21c855a
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-01-04 16:34:03 -0300

    finalizaçao
    projeto finalizado. mas com planos de algumas mudanças

--- javascript/main.js
+++ javascript/main.js
@@ -1,52 +1,67 @@
-window.addEventListener('scroll', onScroll)
+window.addEventListener('scroll', scrollActive);
+const header = document.querySelector('.main-header');
+const main = document.querySelector(main)
 
-onScroll()
 
-function onScroll() {
-  showNaveOnSccroll() 
-  showBackToTopButtonOnScroll()
+function scrollActive() {
+  changeHeader()
+  showButtonGoTop()
+};
 
-} 
+function changeHeader() {
+  if (scrollY > 20) {
+    header.classList.add('-scroll')
+    main.classList.add('-scroll')
+  } else {
+    header.classList.remove('-scroll')
+    main.classList.remove('-scroll')
+  }
+};
 
-function showNaveOnSccroll() {
-  if (scrollY > 0) {     
-      navigation.classList.add('scroll') 
-    } else  {   
-      navigation.classList.remove('scroll')
-    }
-}
+// onScroll()
 
-function showBackToTopButtonOnScroll() {
-  if (scrollY > 550) {     
-      backToTopButton.classList.add('show');
-    } else  {   
-      backToTopButton.classList.remove('show');
-    }
+// function onScroll() {
+//   showNaveOnSccroll() 
+//   showBackToTopButtonOnScroll()
+// } 
 
-}
+// function showNaveOnSccroll() {
+//   if (scrollY > 0) {     
+//       navigation.classList.add('scroll') 
+//     } else  {   
+//       navigation.classList.remove('scroll')
+//     }
+// }
 
-function openMenu() {
-   document.body.classList.add('menu-expanded') 
+// function showBackToTopButtonOnScroll() {
+//   if (scrollY > 550) {     
+//       backToTopButton.classList.add('show');
+//     } else  {   
+//       backToTopButton.classList.remove('show');
+//     }
 
-}
+// }
 
-function closeMenu() {
-  document.body.classList.remove('menu-expanded')
-}
+// function openMenu() {
+//    document.body.classList.add('menu-expanded') 
 
- 
+// }
 
-ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700
-}).reveal(`
-   #home, 
-   #home img,
-   #home .stats,
-   #services,
-   #services header,
-   #services .card
-   #about,
-   about header,
-   #about, .content`)
+// function closeMenu() {
+//   document.body.classList.remove('menu-expanded')
+// }
+
+// ScrollReveal({
+//   origin: 'top',
+//   distance: '30px',
+//   duration: 700
+// }).reveal(`
+//    #home, 
+//    #home img,
+//    #home .stats,
+//    #services,
+//    #services header,
+//    #services .card
+//    #about,
+//    about header,
+//    #about, .content`)
