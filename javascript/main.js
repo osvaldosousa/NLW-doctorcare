7ebc954940a7b4424ad3ef63d60a5654b1caa30b
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-04-17 13:17:45 -0300

    melhorando UI da aplicação

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,12 @@
-const header = document.querySelector('.main-header');
-const main = document.querySelector('main');
-const btnTop = document.querySelector('.btn-goTop');
+const header = document.querySelector('.main-header')
+const main = document.querySelector('main')
+const btnTop = document.querySelector('.btn-goTop')
+const linksTag = document.querySelectorAll('.ul-links')
 
 function scrollActive() {
   changeHeader()
   showButtonGoTop()
-};
+}
 
 function changeHeader() {
   if (scrollY > 20) {
@@ -15,7 +16,7 @@ function changeHeader() {
     header.classList.remove('-scroll')
     main.classList.remove('-scroll')
   }
-};
+}
 
 function showButtonGoTop() {
   if (scrollY >= 450) {
@@ -23,21 +24,29 @@ function showButtonGoTop() {
   } else {
     btnTop.classList.remove('-on')
   }
-};
+}
 
 btnTop.addEventListener('click', () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
 })
 
-window.addEventListener('scroll', scrollActive);
+window.addEventListener('scroll', scrollActive)
 
-let show = true;
-const menuMobile = document.querySelector(".menu");
-const menuToggle = menuMobile.querySelector(".menu-toggle");
+let show = true
+const menuMobile = document.querySelector('.menu')
+const menuToggle = menuMobile.querySelector('.menu-toggle')
 
-menuToggle.addEventListener("click", () => {
-  document.body.style.overflow = show ? "hidden" : "initial"
-  menuMobile.classList.toggle("open", show)
-  show = !show;
-});
+menuToggle.addEventListener('click', () => {
+  document.body.style.overflow = show ? 'hidden' : 'initial'
+  menuMobile.classList.toggle('open', show)
+  show = !show
+})
+
+linksTag.forEach(tag => {
+  tag.addEventListener('click', () => {
+    document.body.style.overflow = 'initial'
+    menuMobile.classList.remove('open', show)
+    show = !show
+  })
+})
